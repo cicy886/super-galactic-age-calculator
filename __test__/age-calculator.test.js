0ecbba3b718e0d1b71d8345dfbd094d29db7d1eb
Sisi Vieira <cicy886@gmail.com>
2021-07-31 14:10:24 -0700

    check user's Mercury age. Test fail. Good fail.

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -2,8 +2,13 @@ import {AgeCalculator} from '../src/age-calculator.js';
 
 describe ('AgeCalculator', () => {
 
-  test('Should correctly receive users age',() =>{
+  test('should correctly receive users age',() =>{
     const ageCalculator= new AgeCalculator (15);
     expect(ageCalculator.age).toEqual(15);
+  });
+
+  test('should correctly calculate users Mercury age',() =>{
+    let mercuryAge = new AgeCalculator (15);
+    expect(mercuryAge.checkMercuryAge()).toEqual(3);
   });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -2,4 +2,8 @@ export class AgeCalculator{
   constructor (age){
     this.age = age;
   }
+
+  checkMercuryAge(){
+    return this.age = Math.round(this.age*0.24);
+  }
 }

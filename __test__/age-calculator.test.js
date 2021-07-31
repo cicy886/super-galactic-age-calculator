e70fd3631cc08e7447cfb14038e877e0dd65cf26
Sisi Vieira <cicy886@gmail.com>
2021-07-31 13:55:45 -0700

    test passed

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -4,6 +4,6 @@ describe ('AgeCalculator', () => {
 
   test('Should correctly receive users age',() =>{
     const ageCalculator= new AgeCalculator (15);
-    expect(ageCalculator.age).toEqual(13);
+    expect(ageCalculator.age).toEqual(15);
   });
 });

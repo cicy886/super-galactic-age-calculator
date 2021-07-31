96af6aafc8a84bef2f9f85ebf8c8a24ce2af9e0b
Sisi Vieira <cicy886@gmail.com>
2021-07-31 15:05:48 -0700

    added name in constructor function, add submmit botton, add UI logic

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -2,28 +2,33 @@ import {AgeCalculator} from '../src/age-calculator.js';
 
 describe ('AgeCalculator', () => {
 
-  test('should correctly receive users age',() =>{
-    const ageCalculator= new AgeCalculator (15);
+  test('should correctly receive users name and age',() =>{
+    const ageCalculator= new AgeCalculator ("Sisi",15);
+    expect(ageCalculator.name).toEqual("Sisi");
     expect(ageCalculator.age).toEqual(15);
   });
 
   test('should correctly calculate users Mercury age',() =>{
-    let mercuryAge = new AgeCalculator (15);
+    let mercuryAge = new AgeCalculator ("Sisi",15);
+    expect(mercuryAge.name).toEqual("Sisi");
     expect(mercuryAge.checkMercuryAge()).toEqual(4);
   });
 
   test('should correctly calculate users Venus age', () =>{
-    let venusAge = new AgeCalculator (15);
+    let venusAge = new AgeCalculator ("Sisi",15);
+    expect(venusAge.name).toEqual("Sisi");
     expect(venusAge.checkVenusAge()).toEqual(9);
   });
 
   test('should correctly calculate users Mars age', () =>{
-    let marsAge = new AgeCalculator (15);
+    let marsAge = new AgeCalculator ("Sisi",15);
+    expect(marsAge.name).toEqual("Sisi");
     expect(marsAge.checkMarsAge()).toEqual(28);
   });
 
   test('should correctly calculate users Jupiter age', () =>{
-    let jupiterAge = new AgeCalculator (15);
+    let jupiterAge = new AgeCalculator ("Sisi",15);
+    expect(jupiterAge.name).toEqual("Sisi");
     expect(jupiterAge.checkJupiterAge()).toEqual(178);
   });
 
--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import './css/styles.css';
 import {AgeCalculator} from './age-calculator.js';
 
 $(document).ready(function() {
+  $('#age-calculator-form').submit(function(event){
+    event.preventDefault();
+    const name = $('#name').val;
+    const earthAge = $('#age').val;
+    const ageCalculator = new AgeCalculator(name,earthAge);
+    $('#display-age').append("<li>" + "Hi " + name + "!" + "</li");
+    const mercuryAge = ageCalculator.checkMercuryAge();
+    $('#display-age').append("<li>" + "Your Merury Age is " + mercuryAge + "</li");
+  });
 });

6698c956820e2b4c2319139d0903578a344d005d
Sisi Vieira <cicy886@gmail.com>
2021-07-31 17:46:47 -0700

    test passed

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -20,7 +20,7 @@ describe ('AgeCalculator', () => {
     let venusAge = new AgeCalculator ("Sisi",105,100);
     expect(venusAge.name).toEqual("Sisi");
     expect(venusAge.checkVenusAge()).toEqual(65);
-    expect(venusAge.remainingVenusLife()).toEqual(1);
+    expect(venusAge.remainingVenusLife()).toEqual(3);
   });
 
   test('should correctly calculate users Mars age', () =>{
